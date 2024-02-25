0ad0b65dfb6787003eeb75e4f8258942d81cc4a3
Ignacio Mascarell <ignacio.mascarell@murciaeduca.es>
2024-02-25 09:58:28 +0100

    Modifico mongo.py para pasarle datos de conexión (URL/IP y puerto) por variables de entorno. Quizás los valores de retorno por defecto deberían ser otros

--- mongo.py
+++ mongo.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
+from os import environ
 
 # TODO: hay que ver cómo apañamos la conexión para que podamos usar variables de entorno
-client = MongoClient('mongodb://127.0.0.1:27017')
+client = MongoClient(f'mongodb://{environ.get("MONGO_IP", "127.0.0.1")}:{environ.get("MONGO_PORT", "27017")}')
 db = client['quacker']
 users = db['users']
 quacks = db['quacks']
